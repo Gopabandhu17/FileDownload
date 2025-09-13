918d278cae361eb8f505cccc8f65490389198b04
Gopabandhu Dash <gopabandhudash17@gmail.com>
2025-09-13 13:41:00 +0530

    Initial Commit

---
+++ FileDownloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FileDownloader
+//
+//  Created by Gopabandhu Dash on 13/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FileDownloader/FileDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileDownloaderApp.swift
+//  FileDownloader
+//
+//  Created by Gopabandhu Dash on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct FileDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
